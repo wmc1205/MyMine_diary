4711971055b3dddeffeefafc47b132ef1ea68924
woomincheol <wmc1205@naver.com>
2021-11-21 22:34:19 +0900

    update,delete 구현

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,8 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create/',views.create, name='create'),
-    path('<int:post_id>/',views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('<int:post_pk>/', views.detail, name='detail'),
+    path('<int:post_pk>/update', views.update, name='update'),
+    path('<int:post_pk>/delete', views.delete, name='delete'),
     ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,6 @@
 
-
-
-#제목 리스트
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 
 from posts.form import PostForm
 from posts.models import Post
@@ -15,19 +11,41 @@ def index(request):
     context = {'post_list': post_list}
     return render(request, 'posts/index.html',context)
 # 내용 상세 페이지
-def detail(request,post_id):
-
-    post = get_object_or_404(Post, pk =post_id)
-    return render(request,'posts/detail.html',{'post':post})
+def detail(request,post_pk):
+    post = get_object_or_404(Post,pk=post_pk)
+    context = {'post': post}
+    return render(request, 'posts/detail.html',context)
 
 #새로운 글 등록
 def create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('index')
-        else:
-            form = PostForm()
-        return render(request,'posts/create.html', {'form':form})
+           post = form.save(commit=False)
+           post.create_date = timezone.now()
+           post.save()
+           return redirect('posts:index')
+    else:
+        form = PostForm()
+    context = {'form': form}
+    return render(request, 'posts/create.html', context)
 
+def update(request,post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.create_date = timezone.now()
+            post.save()
+            return redirect('posts:detail')
+    else:
+        form = PostForm()
+    context = {'form' : form}
+    return render(request, 'posts/update.html',context)
+
+def delete(request,post_pk):
+    post = Post.objects.get(request.Post, pk=post_pk)
+    post.delete()
+    return redirect('posts:index')
