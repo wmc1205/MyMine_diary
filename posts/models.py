8f156b69c9eb5b0dbb0526690d6e3ca2567a7c3b
woomincheol <wmc1205@naver.com>
2022-01-08 14:34:35 +0900

    model 수정

--- posts/models.py
+++ posts/models.py
@@ -8,10 +8,12 @@ from django.utils import timezone
 class Post(models.Model):
     writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post',null=True)
     post_title = models.CharField(max_length=120)
-    post_image = models.ImageField(upload_to='posts/',blank=True,null=True)
+    post_image = models.ImageField(upload_to='posts/',blank=False,null=True)
     post_content = models.TextField(null=True)
     today_date = models.DateTimeField(auto_now_add=True, null=True)
 
+    class Meta:
+        ordering=['today_date']
 
     def publish(self):
         self.today_date = timezone.now()
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,10 @@
 
 
-from django.contrib.auth import get_user_model
+
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.db.models import Q
-from django.http import request
-from django.shortcuts import render, redirect, get_object_or_404
+
 from django.urls import reverse, reverse_lazy
-from django.utils import timezone
+
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
@@ -26,13 +23,16 @@ class PostIndexView(ListView):
     template_name = 'posts/index.html'
     paginate_by = 6
 
+
     #login user와 writer가 일치할 경우에만, post 출력하는 코드 구현
 
     def get_queryset(self):
+
         posts = Post.objects.filter(writer=self.request.user).values_list('writer')
         post_list = Post.objects.filter(writer__in=posts)
-        order=Post.objects.all().order_by('-today_date')
+
         return post_list
+
 
 
     #<eeror 2>
